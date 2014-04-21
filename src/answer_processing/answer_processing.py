47b9aeff1400beb723cd759109993003d9b0586b
Claire Jaja <claire.jaja@gmail.com>
2014-04-21 13:33:45 -0700

    Comment out alpha version of answer generation algorithm.

--- src/answer_processing/answer_processing.py
+++ src/answer_processing/answer_processing.py
@@ -33,27 +33,27 @@ class AnswerProcessor:
             # also increment scores based on something like inverse passage rank
 
         # here's a possible clever answer extractor
-        answer_docs = defaultdict(set)
-        answer_score = defaultdict(lambda:0)
-        for passage in self.passages:
-            for i in range(len(passage.passage)): # is this a string or a list of strings? assuming list
-                answers = []
+#        answer_docs = defaultdict(set)
+#        answer_score = defaultdict(lambda:0)
+#        for passage in self.passages:
+#            for i in range(len(passage.passage)): # is this a string or a list of strings? assuming list
+#                answers = []
                 # unigram
-                answers.append(passage.passage[i])
-                if i < len(passage.passage) - 2: # can do bigrams
-                    answers.append(" ".join(passage.passage[i:i+2]))
-                    if i < len(passage.passage) - 3: # can do trigrams
-                            answers.append(" ".join(passage.passage[i:i+3]))
-                           if i < len(passage.passage) - 4: # can do 4-grams
-                                answers.append(" ".join(passage.passage[i:i+4]))
-                for answer in answers:
-                    answer_docs[answer].add(passage.doc_id)
-                    answer_score[answer] += passage.weight
+#                answers.append(passage.passage[i])
+#               if i < len(passage.passage) - 2: # can do bigrams
+#                    answers.append(" ".join(passage.passage[i:i+2]))
+#                    if i < len(passage.passage) - 3: # can do trigrams
+#                            answers.append(" ".join(passage.passage[i:i+3]))
+#                           if i < len(passage.passage) - 4: # can do 4-grams
+#                                answers.append(" ".join(passage.passage[i:i+4]))
+#                for answer in answers:
+#                    answer_docs[answer].add(passage.doc_id)
+#                    answer_score[answer] += passage.weight
         # then find answers with highest score?
-        answer_score_list = []
-        for answer,score in answer_score.iteritems():
-            answer_score_list.append((answer,score))
-        sorted_answers = sorted(answer_score_list,key=itemgetter(1))
+#        answer_score_list = []
+#        for answer,score in answer_score.iteritems():
+#            answer_score_list.append((answer,score))
+#        sorted_answers = sorted(answer_score_list,key=itemgetter(1))
         # remove words from original question and stop words
 
     # a method to check candidate answers against the answer template
