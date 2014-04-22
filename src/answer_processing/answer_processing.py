0f129ace450c1415d0e52d0a49dbe2511c0f77f7
Claire Jaja <claire.jaja@gmail.com>
2014-04-22 12:31:05 -0700

    Add method for extracting n-gram answers from passages.  Add method for combining n-gram answer scores.

--- src/answer_processing/answer_processing.py
+++ src/answer_processing/answer_processing.py
@@ -1,5 +1,5 @@
 # LING 573 Question Answering System
-# Code last updated 4/18/14 by Claire Jaja
+# Code last updated 4/22/14 by Claire Jaja
 # This code implements an Answer Processor for the question answering system.
 
 from general_classes import AnswerTemplate, Passage
@@ -32,29 +32,54 @@ class AnswerProcessor:
             # later, do something more clever, like count n-grams
             # also increment scores based on something like inverse passage rank
 
+    def extract_answers2(self):
         # here's a possible clever answer extractor
-#        answer_docs = defaultdict(set)
-#        answer_score = defaultdict(lambda:0)
-#        for passage in self.passages:
-#            for i in range(len(passage.passage)): # is this a string or a list of strings? assuming list
-#                answers = []
+        answer_docs = defaultdict(set)
+        answer_score = defaultdict(lambda:0)
+        for passage in self.passages:
+            passage_list = passage.passage.split()
+            print passage_list, passage.weight
+            for i in range(len(passage_list)):
+                answers = []
                 # unigram
-#                answers.append(passage.passage[i])
-#               if i < len(passage.passage) - 2: # can do bigrams
-#                    answers.append(" ".join(passage.passage[i:i+2]))
-#                    if i < len(passage.passage) - 3: # can do trigrams
-#                            answers.append(" ".join(passage.passage[i:i+3]))
-#                           if i < len(passage.passage) - 4: # can do 4-grams
-#                                answers.append(" ".join(passage.passage[i:i+4]))
-#                for answer in answers:
-#                    answer_docs[answer].add(passage.doc_id)
-#                    answer_score[answer] += passage.weight
-        # then find answers with highest score?
-#        answer_score_list = []
-#        for answer,score in answer_score.iteritems():
-#            answer_score_list.append((answer,score))
-#        sorted_answers = sorted(answer_score_list,key=itemgetter(1))
+                answers.append(passage_list[i])
+                if i < len(passage_list) - 1: # can do bigrams
+                    answers.append(" ".join(passage_list[i:i+2]))
+                    if i < len(passage_list) - 2: # can do trigrams
+                        answers.append(" ".join(passage_list[i:i+3]))
+                        if i < len(passage_list) - 3: # can do 4-grams
+                            answers.append(" ".join(passage_list[i:i+4]))
+                for answer in answers:
+                    answer_docs[answer].add(passage.doc_id)
+                    # passage weight is negative - closer to 0 is better
+                    # change to positive, then take inverse
+                    # higher score is still better, but now will be all positive
+                    answer_score[answer] += (-passage.weight)**-1
+       # then find answers with highest score?
+        answer_score_list = []
+        for answer,score in answer_score.iteritems():
+            ac = AnswerCandidate(answer,answer_docs[answer])
+            ac.set_score(score)
+            answer_score_list.append(ac)
+        #sorted_answers = sorted(answer_score_list,reverse=True,key=itemgetter(1))
         # remove words from original question and stop words
+
+        self.combine_answers(answer_score_list)
+        sorted_answers = sorted(answer_score_list,reverse=True,key=attrgetter('score'))
+        return sorted_answers
+
+
+    # combine answer candidates so that unigrams aren't highest
+    def combine_answers(self,answers):
+        # update score of each answer
+        # to be current score + sum of scores of answer unigrams in it
+        for answer in answers:
+            if len(answer.answer.split()) > 1: # only update non-unigram scores
+                new_score = answer.score
+                for other_answer in answers:
+                    if len(other_answer.answer.split()) == 1 and other_answer.answer in answer.answer:
+                        new_score += other_answer.score
+                answer.set_score(new_score) 
 
     # a method to check candidate answers against the answer template
     def reweight_answers(self):
@@ -79,3 +104,6 @@ class AnswerCandidate:
 
     def set_score(self,score):
         self.score = score
+
+    def __repr__(self):
+        return "'%s' from %s with score %s" % (self.answer, self.doc_id, self.score)
