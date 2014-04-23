69ab545fa32f06dcf19797e0f0249f122d1622e7
Clara Gordon <cgordon1@patas.ling.washington.edu>
2014-04-23 14:13:50 -0700

    Add stopword arg to indrirunquery command

--- src/info_retrieval/info_retrieval.py
+++ src/info_retrieval/info_retrieval.py
@@ -32,7 +32,8 @@ class InfoRetriever:
             try:
                 args = [self.indri_loc,'-query=#combine(' + query + ')', 
                        '-index=' + self.path_to_idx, '-printSnippets=true',
-                         '-trecFormat=true', '-printSnippets=true', '-count=20']
+                         '-trecFormat=true', '-printSnippets=true', '-count=20'
+			 '-stopper=./stoplist.dft']
                 p1 = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 results = p1.communicate()[0][2:]
             except:
