d1d90bc6aff48602db87fccf8f1469bcfbe26d0e
amkahn <amkahn@uw.edu>
2014-04-25 17:20:36 -0700

    Attempt to push outputs directory

--- D2.cmd
+++ D2.cmd
@@ -1,8 +1,10 @@
 question_file = /dropbox/13-14/573/Data/Questions/devtest/TREC-2006.xml
-doc_index = /home2/cjaja/classwork/spring-2014/ling573/question-answering/src/index
+doc_index = /home2/cjaja/classwork/spring-2014/ling573/question-answering/src/index_krovetz
+run_tag = D2
+output_file = outputs/D2.outputs
 
 executable = src/question_answering.sh
-arguments = $(question_file) $(doc_index) D2 outputs/D2.outputs
+arguments = $(question_file) $(doc_index) $(run_tag) $(output_file)
 Universe = vanilla
 getenv = true
 output = D2.out
