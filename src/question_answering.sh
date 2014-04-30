c178d8985629a9077683f932475d17bdb42bc6e2
Clara Gordon <cgordon1@patas.ling.washington.edu>
2014-04-30 14:58:58 -0700

    Ran first trial of new IR module.

--- src/question_answering.sh
+++ src/question_answering.sh
@@ -7,7 +7,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $DIR/question_answering.py $@
 
 # Run the MRR eval script with strict evaluation, redirecting output to the appropriate results file
-python2.6 /dropbox/13-14/573/code/compute_mrr.py /dropbox/13-14/573/Data/patterns/devtest/factoid-docs.litkowski.2006.txt $4 strict > $DIR/../results/D2.results_strict
+tython2.6 /dropbox/13-14/573/code/compute_mrr.py /dropbox/13-14/573/Data/patterns/devtest/factoid-docs.litkowski.2006.txt $4 strict > $DIR/../results/test/results_strict
 
 # Run the MRR eval script with lenient evaluation, redirecting output to the appropriate results file
-python2.6 /dropbox/13-14/573/code/compute_mrr.py /dropbox/13-14/573/Data/patterns/devtest/factoid-docs.litkowski.2006.txt $4 lenient > $DIR/../results/D2.results_lenient
+python2.6 /dropbox/13-14/573/code/compute_mrr.py /dropbox/13-14/573/Data/patterns/devtest/factoid-docs.litkowski.2006.txt $4 lenient > $DIR/../results/test/results_lenient
+
+
+
