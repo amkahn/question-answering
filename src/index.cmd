9f9407485d51fd0181f30f9faf2ef0e86a4a56c8
Claire Jaja <claire.jaja@gmail.com>
2014-05-02 10:34:19 -0700

    Add new (faster) way to index, now t hrough Condor submit script using Indri parameter file, not through Python.

---
+++ src/index.cmd
@@ -0,0 +1,12 @@
+# Claire Jaja
+# 5/2/2014
+# LING 573 Question Answering
+# index documents
+
+Executable = /NLP_TOOLS/info_retrieval/indri/latest/bin/IndriBuildIndex
+Output = index.out
+Error = index.err
+Log = index.log
+Arguments = "index_parameters.xml"
+transfer_executable = false
+Queue
