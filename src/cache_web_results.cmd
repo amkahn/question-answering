6cead29f2bbcdcf33530dc546fb322dd121546c9
Claire Jaja <claire.jaja@gmail.com>
2014-05-10 13:57:07 -0700

    Delete Condor files from previous runs. Minor update to Condor submit script for caching web results.

--- src/cache_web_results.cmd
+++ src/cache_web_results.cmd
@@ -3,13 +3,13 @@
 # LING 573 Question Answering
 # cache web results
 
+q_name = TREC-2006
 q_file = /dropbox/13-14/573/Data/Questions/devtest/TREC-2006.xml
-out_file = cached_web_results/TREC-2006.web_cache
 
 Executable = cached_web_results/cache_web_results.py
-Output = cached_web_results/cache_web_results.$(q_file).out
-Error = cached_web_results/cache_web_results.$(q_file).err
-Log = cached_web_results/cache_web_results.$(q_file).log
-Arguments = "$(q_file) $(out_file)"
+Output = cached_web_results/cache_web_results.$(q_name).out
+Error = cached_web_results/cache_web_results.$(q_name).err
+Log = cached_web_results/cache_web_results.$(q_name).log
+Arguments = "$(q_file) cached_web_results/$(q_name).web_cache"
 transfer_executable = false
 Queue
