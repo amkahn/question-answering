505e15d40c68fd6e76268bf46c5aff12265f5b28
amkahn <amkahn@uw.edu>
2014-05-30 20:06:16 -0700

    Changed index path to absolute path for evaltest

--- D4.cmd
+++ D4.cmd
@@ -17,7 +17,7 @@ Queue
 run_tag = 2007
 q_file = /dropbox/13-14/573/Data/Questions/evaltest/QA2007_testset.xml
 web_cache = src/cached_web_results/QA2007_testset.3pg.web_cache
-index = src/indexes/AQUAINT-2/porter.stoplist
+index = /home2/cjaja/classwork/spring-2014/ling573/question-answering/src/indexes/AQUAINT-2/porter.stoplist
 
 executable = src/question_answering_evaltest.sh
 arguments = $(run_tag) q_file=$(q_file) web_cache=$(web_cache) index=$(index) indri_passages=$(indri_passages) passage_length=$(passage_length) indri_window_size=$(indri_window_size)
