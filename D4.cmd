6ab948e3039486e05d0f4baac2af1eeaec1d053b
Clara Gordon <cgordon1@patas.ling.washington.edu>
2014-05-30 19:23:24 -0700

    Final D4.cmd edits

--- D4.cmd
+++ D4.cmd
@@ -1,8 +1,10 @@
 run_tag = 2006
-# params
- 
+indri_passages = 20
+passage_length = 75
+indri_window_size = 30
+
 executable = src/question_answering.sh
-arguments = $(run_tag)
+arguments = $(run_tag) indri_passages=$(indri_passages) passage_length=$(passage_length) indri_window_size=$(indri_window_size)
 Universe = vanilla
 getenv = true
 output = $(run_tag).out
@@ -16,9 +18,8 @@ run_tag = 2007
 q_file = /dropbox/13-14/573/Data/Questions/evaltest/QA2007_testset.xml
 web_cache = src/cached_web_results/QA2007_testset.3pg.web_cache
 index = src/indexes/AQUAINT-2/porter.stoplist
-# params
 
 executable = src/question_answering_evaltest.sh
-arguments = $(run_tag)
+arguments = $(run_tag) q_file=$(q_file) web_cache=$(web_cache) index=$(index) indri_passages=$(indri_passages) passage_length=$(passage_length) indri_window_size=$(indri_window_size)
 
 Queue
